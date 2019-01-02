ca43f93f2d3d19103ab874e1699f8792c1b9f8b6
onielrosario <onielrosario@pursuit.org>
2019-01-02 16:16:34 -0500

    fixed fonts

--- JanAssaignment/IGNViewController.swift
+++ JanAssaignment/IGNViewController.swift
@@ -24,20 +24,18 @@ class IGNViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //SFSafariViewController = self
         tableView.dataSource = self
         tableView.delegate = self
+        searBarch.delegate = self
         IgnAPIClient.getArticles { (articles, error) in
             if let error = error {
                 print("error: \(error)")
             }
             if let articles = articles {
-            self.articles = articles
+                self.articles = articles
             }
         }
         title = "🎮 Gaming News 🎮"
-        
     }
     
     @objc func tapFunction(sender: TapGestureRecognizer) {
@@ -49,41 +47,43 @@ class IGNViewController: UIViewController {
     
 }
 func convertDate(str: String) -> String {
-    // "2018-12-18T06:06:35Z"
-var arr = str.components(separatedBy: "T")
-let string = arr[0]
+    var arr = str.components(separatedBy: "T")
+    let string = arr[0]
     let dateFormater = DateFormatter()
     dateFormater.dateFormat = "yyyy-MM-dd"
     let date = dateFormater.date(from: string) // Date
     dateFormater.dateFormat = "MMMM dd, yyyy"
-   let finalStr = dateFormater.string(from: date!)
+    let finalStr = dateFormater.string(from: date!)
     return finalStr
 }
 
 extension IGNViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return articles.count
+        return articles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as? CustomTableViewCell else { return UITableViewCell()}
         let indexpath = articles[indexPath.row]
-       cell.headlineLabel.text = indexpath.title
+        cell.headlineLabel.text = indexpath.title
+        cell.headlineLabel.textColor = .white
+        cell.backgroundColor = UIColor(displayP3Red: CGFloat(101)/255, green: CGFloat(16)/255, blue: CGFloat(32)/255, alpha: 1)
         cell.articleImage.image = IgnAPIClient.getImage(url: indexpath.urlToImage)
         if indexpath.author != nil {
+            cell.articleAuthor.textColor = .white
             cell.articleAuthor.text = "Author: \(indexpath.author ?? "Anonymous")"
         }
+        
         cell.articleDescription.text = indexpath.description
+        
         cell.articleDate.text = convertDate(str: indexpath.publishedAt)
+        cell.articleDate.textColor = .white
         cell.articleLink.isUserInteractionEnabled = true
-        cell.articleLink.text = ">>Click for more info<<"
-        cell.articleLink.textColor = .blue
-        //found a method to make labels clickable
-        //source: https://stackoverflow.com/questions/33658521/how-to-make-a-uilabel-clickable
+        cell.articleLink.text = ">>Click for full Article<<"
+        cell.articleLink.textColor = .orange
         let tap = TapGestureRecognizer(target: self, action: #selector(IGNViewController.tapFunction))
-         tap.url = indexpath.url
+        tap.url = indexpath.url
         cell.articleLink.addGestureRecognizer(tap)
-//        tapFunction(sender: tap, url: indexpath.url)
         return cell
     }
 }
@@ -95,7 +95,8 @@ extension IGNViewController: UITableViewDelegate {
 }
 
 extension IGNViewController: SFSafariViewControllerDelegate {
-    
-    
 }
 
+extension IGNViewController: UISearchBarDelegate {
+    
+}
